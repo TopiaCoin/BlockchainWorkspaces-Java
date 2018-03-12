d18f7e00ad1e7c68b496a41c29bbe83653d7c035
Cody Sandwith <csandwith@topiatechnology.com>
2018-03-12 09:45:51 -0700

    Updated negative test pattern

--- core/src/test/java/io/topiacoin/core/impl/DefaultConfigurationTest.java
+++ core/src/test/java/io/topiacoin/core/impl/DefaultConfigurationTest.java
@@ -14,43 +14,35 @@ public class DefaultConfigurationTest {
 		String theDefaultValueOfThatProperty = "bar";
 		Configuration conf = new DefaultConfiguration();
 		//Negative tests
-		boolean shouldFail = true;
 		try {
 			conf.getConfigurationOption(null);
+			fail("Expected an IllegalArgumentException to be thrown, but it wasn't");
 		} catch(IllegalArgumentException ex) {
-			shouldFail = false;
 		}
-		assertTrue("Expected an IllegalArgumentException to be thrown, but it wasn't", !shouldFail);
-		shouldFail = true;
 		try {
 			conf.getConfigurationOption("");
+			fail("Expected an IllegalArgumentException to be thrown, but it wasn't");
 		} catch(IllegalArgumentException ex) {
-			shouldFail = false;
 		}
-		assertTrue("Expected an IllegalArgumentException to be thrown, but it wasn't", !shouldFail);
 		String val = conf.getConfigurationOption(oneOfTheDefaultPropertyNames);
 		assertEquals("Property Values do not match...did the default config change?", theDefaultValueOfThatProperty, val);
 		val = conf.getConfigurationOption("i am a potato");
 		assertEquals("Expected non-existant confg option to be null, wasn't", null, val);
-		shouldFail = true;
 		try {
 			conf.setConfigurationOption("testProp", "test");
+			fail("Expected a NotImplementedException to be thrown because Notifications haven't been implemented yet...did you implement them?");
 		} catch(NotImplementedException ex) {
-			shouldFail = false;
 		}
-		assertTrue("Expected a NotImplementedException to be thrown because Notifications haven't been implemented yet...did you implement them?", !shouldFail);
 		val = conf.getConfigurationOption("testProp");
 		assertEquals("Property Values do not match...set didn't work?", "test", val);
 		//This one shouldn't throw a NotImplementedException, because the value didn't change
 		conf.setConfigurationOption("testProp", "test");
 		assertEquals("Property Values do not match...set didn't work?", "test", val);
-		shouldFail = true;
 		try {
 			conf.setConfigurationOption("testProp", null);
+			fail("Expected a NotImplementedException to be thrown because Notifications haven't been implemented yet...did you implement them?");
 		} catch(NotImplementedException ex2) {
-			shouldFail = false;
 		}
-		assertTrue("Expected a NotImplementedException to be thrown because Notifications haven't been implemented yet...did you implement them?", !shouldFail);
 		val = conf.getConfigurationOption("testProp");
 		assertEquals("Property Values do not match...set didn't work?", null, val);
 	}
